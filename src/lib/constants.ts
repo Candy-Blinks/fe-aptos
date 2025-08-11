e7effb64fcadbaa3de19c40c8039d9b53d1cb9d5
JohnPaulPabelico <pabelicojohnpaul@gmail.com>
2025-08-11 17:16:52 +0800

    oops

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,17 +1,17 @@
 import type { Network } from "@aptos-labs/wallet-adapter-react";
-// import { PinataSDK } from "pinata-web3";
+import { PinataSDK } from "pinata-web3";
 
 export const NETWORK: Network = (process.env.NEXT_PUBLIC_APP_NETWORK as Network) ?? "testnet";
-// export const MODULE_ADDRESS = process.env.NEXT_PUBLIC_MODULE_ADDRESS;
+export const MODULE_ADDRESS = process.env.NEXT_PUBLIC_MODULE_ADDRESS;
 export const APTOS_API_KEY = process.env.NEXT_PUBLIC_APTOS_API_KEY;
 
 export const ASSETS_URL = "https://raw.githubusercontent.com/Candy-Blinks/assets/refs/heads/main/";
 
-// export const API_URL = process.env.NEXT_PUBLIC_API_URL ?? "";
-// export const PINATA_JWT = process.env.NEXT_PUBLIC_PINATA_JWT ?? "TEST";
-// export const PINATA_GATEWAY = process.env.NEXT_PUBLIC_PINATA_GATEWAY ?? "https://gateway.pinit.io/";
+export const API_URL = process.env.NEXT_PUBLIC_API_URL ?? "";
+export const PINATA_JWT = process.env.NEXT_PUBLIC_PINATA_JWT ?? "TEST";
+export const PINATA_GATEWAY = process.env.NEXT_PUBLIC_PINATA_GATEWAY ?? "https://gateway.pinit.io/";
 
-// export const PINATA = new PinataSDK({
-//   pinataJwt: PINATA_JWT,
-//   pinataGateway: PINATA_GATEWAY,
-// });
+export const PINATA = new PinataSDK({
+  pinataJwt: PINATA_JWT,
+  pinataGateway: PINATA_GATEWAY,
+});
